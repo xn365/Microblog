7c888a47fba72621208274b8a0386dd3910ce336
xn_365@163.com <xn_365@163.com>
2018-10-25 15:43:04 +0800

    fix user login bug.

--- app/routes.py
+++ app/routes.py
@@ -65,12 +65,14 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    if username == current_user.username:
+        user = User.query.filter_by(username=username).first_or_404()
+        posts = [
+            {'author': user, 'body': 'Test post #1'},
+            {'author': user, 'body': 'Test post #2'}
+        ]
+        return render_template('user.html', user=user, posts=posts)
+    return redirect(url_for('logout'))
 
 @app.before_request
 def before_request():
