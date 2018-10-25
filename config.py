fb3aabdf4c21d062043ded5f8ce74fa121ee3248
xn_365@163.com <xn_365@163.com>
2018-10-25 16:57:57 +0800

    Error Handing

--- config.py
+++ config.py
@@ -7,3 +7,15 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #MAIL_SERVER = 'smtp.163.com'
+    #MAIL_PORT = 25
+    #MAIL_USE_TLS = True 
+    #MAIL_USERNAME = 'xn_365'
+    #MAIL_PASSWORD = '1QAZsmtp111111'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['xn_365@163.com']
