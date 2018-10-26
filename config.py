9a4dd20caf0e6d37064385d057b2606b2f3e6776
xn_365@163.com <xn_365@163.com>
2018-10-26 16:31:04 +0800

    Pagination

--- config.py
+++ config.py
@@ -8,14 +8,11 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    #MAIL_SERVER = 'smtp.163.com'
-    #MAIL_PORT = 25
-    #MAIL_USE_TLS = True 
-    #MAIL_USERNAME = 'xn_365'
-    #MAIL_PASSWORD = '1QAZsmtp111111'
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['xn_365@163.com']
+
+    POSTS_PER_PAGE = 3
