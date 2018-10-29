92ac64a366b72e162b7e1e4b5c7139b41648edf0
xn_365@163.com <xn_365@163.com>
2018-10-29 10:01:22 +0800

    Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from logging.handlers import RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER'] and app.config['MAIL_SERVER'] != 'localhost':
